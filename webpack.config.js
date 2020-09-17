72c9bc35fb9a38e1bbc4dc47a7bfc7bafab96765
Santiago Landaburo <santilandaburo73@gmail.com>
2020-09-17 20:20:19 -0300

    modificacion webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './index.js',
@@ -28,11 +28,11 @@ module.exports = {
             template: './public/index.html',
             filname: './index.html'
         }),
-        // new CopyWebpackPlugin({
-        //     patterns: [
-        //       { from: './src/style/styles.css', to: '' },
-        //     ],
-        //   }),
+        new CopyWebpackPlugin({
+            patterns: [
+              { from: './src/style/styles.css', to: '' },
+            ],
+          }),
     
     ]
 }
